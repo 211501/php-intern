b75fc012a097af56326bada358e641c3c0cd349b
Anh Bao <lephuanhbao1042001@gmail.com>
2023-12-14 10:41:48 +0700

    update

--- database/intern.sql
+++ database/intern.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 21, 2023 at 04:00 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 14, 2023 lúc 04:28 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `intern`
+-- Cơ sở dữ liệu: `intern`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Cấu trúc bảng cho bảng `employee`
 --
 
 CREATE TABLE `employee` (
@@ -34,7 +34,7 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `employee`
+-- Đang đổ dữ liệu cho bảng `employee`
 --
 
 INSERT INTO `employee` (`id`, `name`, `hash_id`) VALUES
@@ -46,7 +46,7 @@ INSERT INTO `employee` (`id`, `name`, `hash_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Cấu trúc bảng cho bảng `user`
 --
 
 CREATE TABLE `user` (
@@ -61,48 +61,43 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Đang đổ dữ liệu cho bảng `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `pwd`, `userType`, `email`, `description`, `token`, `hashed_data`) VALUES
-(1, 'baoLe', 'Q1234', 'admin', 'lebao123@gmail.com', 'ok123', 'c9733c5c3f4e5bdce238e35ff3b3ce7122c86998e23b04ab095c9927959e125b', NULL),
-(336, 'test15', 'Q1234', 'user', 'bao15@gmail.com', '', '320baa298c96625ff0a1900c816ce8678e07091c63cd14e536570159dde3e4da', NULL),
-(337, 'test16', 'Q1234', 'user', 'bao16@gmail.com', '', 'cc3412ad3c8cce0f0551e00538b81cae3870adcae23d7c06cbfadeb70de385fa', NULL),
-(338, 'test17', 'Q1234', 'user', 'bao17@gmail.com', '', 'd09f989d50a7efa147e96f54070e94707555b8c1faec821964a3fe854b88b654', NULL),
-(339, 'test18', 'Q1234', 'user', 'bao18@gmail.com', '', 'abedc720b5d8aa0fa79939a37fb97483b53a883ea9c1f1adfafcc615a35e03c9', NULL),
-(340, 'test19', 'Q1234', 'user', 'bao19@gmail.com', '', 'f8d909051eede224cac7add4f772831d2d52c50d1675490c57faa334447731b8', NULL),
-(342, 'fake', 'Q1234', 'admin', 'fake@gmail.com', '', '6556f55207a74d393f55a81f3940c646e2d65e60830f8e6e3e300d604ea54e8e', NULL),
-(456, 'bao456', 'Q12345', 'admin', 'bao456@gmail.com', '', '6075290a62bfbed293cb09fa067713bf0d233b56d3f9aade22ee0ea1df677f12', NULL),
-(457, 'bao_anh', 'Q1234', 'admin', 'anhbao.lampart@gmail.com', '<img src=\"http://php.local/deleteUser.php?id=NDYw&token=077734c4da7628527a26616649e67b6fa87abbcd6905a50427d674d8907cadae\">', '04600fc9a4b16ae1e1401bf6c5e2b73b63dbdce705a1eb305fee2cbe5c60aa2f', NULL),
-(460, 'test123', 'Q1234', 'admin', 'bao1@gmail.com', '', '25f3f011026689315f1343cb63999f174a06040f6f57e64d425033c4120a19f0', NULL),
-(473, 'test', 'testA', 'admin', 'test@gmail.com', '', NULL, NULL),
-(474, 'bao', 'testA', 'admin', 'bao@gmail.com', '', NULL, NULL);
+(476, 'anhBao', 'd63fe187906006f1dd9d0292e5a20cbf4b8e2480d4007d65c8f399f333e5aa3d', 'admin', 'anhBao@gmail.com', '', NULL, 'e73cb135243c08ab2c2adc333b150b9237093315f6b38e3361f07caf2bfb4d6b'),
+(477, 'Khoi123', '4b6ffdd74c02baca6e0f6c0396e6a539fc8e935d404fd758c07b6e27d86e4827', 'admin', 'Khoi123@gmail.com', '', NULL, '6e82b8197ce29396936a07b1eb951c88650a2fc0fe1201a51b15b6ca8a73318a'),
+(478, 'Test1', '8a863b145dc6e4ed7ac41c08f7536c476ebac7509e028ed2b49f8bd5a3562b9f', 'admin', 'Test1@gmail.com', '', NULL, '200dd69b70a88134b3a939de5f0b10c44a1675344329b9d9a5ad6b7342f978b2'),
+(479, 'Test2', '32e6e1e134f9cc8f14b05925667c118d19244aebce442d6fecd2ac38cdc97649', 'admin', 'Test2@gmail.com', '', NULL, '9869a8a3a11a33284dc2bcc3d2e6ffd52cad30e2009c11dfe604e74dc21a887e'),
+(480, 'Test3', '68235f4551b9c6423df2af7ead63c90cdd4201ac08525bc3a41cd4755c6c86cb', 'user', 'Test3@gmail.com', '', NULL, 'ddfe0e8d462af661f81db36589c39882dc0f2330785b5d80cd34f2f520ad618f'),
+(481, 'Test4', '68235f4551b9c6423df2af7ead63c90cdd4201ac08525bc3a41cd4755c6c86cb', 'user', 'Test4@gmail.com', '', NULL, '51d089cdaf0c968c94b80671489d22b6f79b1c57de80df880b008e9b37b49788'),
+(482, 'Test5', 'f9ca438226aae2a1aa8503da8fe3aa57aca7036ca0ad8a2a381bec25e188c48b', 'user', 'Test5@gmail.com', '', NULL, 'd4679c618f1af07ee8570edd4b931e2e68e1c2d4b7d3c2f1033a9b597f85d4b0');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `employee`
+-- Chỉ mục cho bảng `employee`
 --
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Chỉ mục cho bảng `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=475;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=483;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
